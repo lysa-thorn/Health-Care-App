d0d7ba1c74beadfcca6edfc38e67d9f67575d8c2
daduon <da.duon1997@gmail>
2022-11-12 13:12:47 +0700

    update materials detail

--- app/components/MaterialDetail.js
+++ app/components/MaterialDetail.js
@@ -1,60 +1,123 @@
-import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet, Image,TouchableOpacity } from "react-native";
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import React from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 
-const MaterialDetail = ({ route, navigation }) => {
+const MaterialDetail = ({ route,navigation }) => {
     const { item } = route.params;
+    const materials ={
+        title:item.name,
+        image:item.image,
+        des:item.description,
+        createDate:item.created_at
+    };
+
+    const user ={
+        fullname:item.user.fullname,
+        image:item.user.image,
+    }
 
     return (
         <View style={styles.container}>
-            <View style={styles.btn}>
-                <TouchableOpacity onPress={() => navigation.navigate("MaterialList")}>
-                    <Icon name="home" size={30} color="#050505" />
-                </TouchableOpacity>
+        <TouchableOpacity style={styles.btnBack} onPress={()=>navigation.navigate('MaterialList')}>
+          <Text style={styles.textBtn}>{'Back'}</Text>
+        </TouchableOpacity>
+        <View style={styles.card}>
+          <View style={styles.cardheader}>
+            <Text style={styles.title}>
+              {materials.title}
+            </Text>
+          </View>
+          <View style={styles.cardBody}>
+            <View>
+              <Image
+                style={styles.image}
+                source={{
+                  uri: materials.image,
+                }}
+              />
             </View>
-            <View style={styles.card}>
-                <View style={styles.cardheader}>
-                    <Text style={styles.title}>{item.name}</Text>
-                </View>
-                <View style={styles.cardBody}>
-                    <Image style={styles.image} source={{ uri: item.image }} />
-                    <Text style={styles.text}>{item.description}</Text>
-                </View>
+            <View>
+                <Image
+                  style={styles.imageProfile}
+                  source={{
+                    uri: user.image,
+                  }}
+                />
             </View>
+            <View>
+                <Text style={styles.text}>Author: {user.fullname}</Text>
+                <Text style={styles.text}>Date: {materials.createDate}</Text>
+            </View>
+            <View style={styles.des}>
+              <Text style={styles.text}>
+                {
+                  materials.des
+                }
+              </Text>
+            </View>
+          </View>
         </View>
+      </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        marginTop: 20,
+        backgroundColor:'#fff'
     },
     card: {
-        padding: 5
+        padding: 5,
+        marginTop: 20,
     },
     cardBody: {
-        padding: 5
+        marginBottom: 30,
     },
-    cardheader: {
-        padding: 5
-    },
+    cardheader: {},
     title: {
+        fontSize: 20,
         color: '#000',
-        fontSize: 24,
+        fontWeight:'600'
     },
     text: {
         color: '#000',
-        fontSize: 15,
+        fontSize: 16,
     },
     image: {
         width: '100%',
         height: 200,
         marginTop: 10,
-        marginBottom: 10
+        marginBottom: 10,
+        borderRadius: 10,
     },
-    btn: {
-        padding: 10
+    imageProfile: {
+        width: 50,
+        height: 50,
+        marginTop: 10,
+        marginBottom: 10,
+        borderRadius: 50,
+        marginRight: 20,
+    },
+    btnBack: {
+        marginTop: 10,
+        padding: 3,
+        marginLeft: 5,
+        backgroundColor: '#04AA6D',
+        width: 70,
+        textAlign: 'center',
+        borderRadius: 10,
+        shadowColor: '#171717',
+        shadowOffset: { width: 0, height: 4 },
+        shadowOpacity: 0.2,
+        shadowRadius: 4,
+    },
+    des:{
+        marginTop:10,
+    },
+    textBtn:{
+        color:'#fff',
+        padding:5,
+        fontSize:16,
+        fontWeight:'500',
+        textAlign:'center'
     }
 });
 
