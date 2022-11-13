7aed080bd5fb4d81fdb5ae4d6c92fefc7ff3d636
lysa-thorn <lysa.thorn@gmail.com>
2022-11-13 13:47:25 +0700

    update material datial

--- app/components/MaterialDetail.js
+++ app/components/MaterialDetail.js
@@ -1,124 +1,202 @@
-import React from "react";
-import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import React, { useState, useEffect } from "react";
+import { 
+  View, 
+  Text, 
+  StyleSheet, 
+  Image, 
+  TouchableOpacity, 
+  ScrollView, 
+  SafeAreaView,
+  TextInput,
+  Button,
+} from "react-native";
+import url from '../url.json';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-const MaterialDetail = ({ route,navigation }) => {
-    const { item } = route.params;
-    const materials ={
-        title:item.name,
-        image:item.image,
-        des:item.description,
-        createDate:item.created_at
-    };
+const MaterialDetail = ({ route, navigation }) => {
+  const { item } = route.params;
+  const [material, setMaterail] = useState([]);
+  const [comment, setComment] = useState([]);
+  const [user, setUser] = useState({})
 
-    const user ={
-        fullname:item.user.fullname,
-        image:item.user.image,
+  const fetchMaterial = async () => {
+    try {
+      const response = await fetch(
+        url.base_url + '/api/materials/' + item.id
+      );
+
+      const getProduct = await response.json();
+      setMaterail(getProduct.materail);
+      setUser(getProduct.materail.user);
+      setComment(getProduct.materail.comments)
+    } catch (error) {
+      console.error(error);
     }
 
-    return (
-        <View style={styles.container}>
-        <TouchableOpacity style={styles.btnBack} onPress={()=>navigation.navigate('MaterialList')}>
-          <Text style={styles.textBtn}>{'Back'}</Text>
-        </TouchableOpacity>
-        <View style={styles.card}>
-          <View style={styles.cardheader}>
-            <Text style={styles.title}>
-              {materials.title}
-            </Text>
+  };
+
+  useEffect(() => {
+    fetchMaterial();
+
+  }, []);
+  return (
+    <SafeAreaView style={styles.container}>
+    <ScrollView style={styles.scrollView}>
+    <View>
+      <TouchableOpacity onPress={() => navigation.navigate('MaterialList')}>
+        <Icon name="arrow-back" size={30}  />
+      </TouchableOpacity>
+      <View style={styles.card}>
+        <View style={styles.cardheader}>
+          <Text style={styles.title}>
+            {material.name}
+          </Text>
+        </View>
+        <View style={styles.cardBody}>
+          <View>
+            <Image
+              style={styles.image}
+              source={{
+                uri: material.image,
+              }}
+            />
           </View>
-          <View style={styles.cardBody}>
-            <View>
-              <Image
-                style={styles.image}
-                source={{
-                  uri: materials.image,
-                }}
-              />
-            </View>
-            <View>
-                <Image
-                  style={styles.imageProfile}
-                  source={{
-                    uri: user.image,
-                  }}
-                />
-            </View>
-            <View>
-                <Text style={styles.text}>Author: {user.fullname}</Text>
-                <Text style={styles.text}>Date: {materials.createDate}</Text>
-            </View>
-            <View style={styles.des}>
-              <Text style={styles.text}>
-                {
-                  materials.des
-                }
-              </Text>
-            </View>
+          <View>
+            <Image
+              style={styles.imageProfile}
+              source={{
+                uri: user.image,
+              }}
+            />
+          </View>
+          <View>
+            <Text style={styles.text}>Author: {user.fullname}</Text>
+            <Text style={styles.text}>Date: {material.created_at}</Text>
+          </View>
+          <View style={styles.des}>
+            <Text style={styles.text}>
+              {
+                material.description
+              }
+            </Text>
+            
           </View>
         </View>
+        
       </View>
-    )
+        <View style={styles.row}>
+          <TextInput
+              style={styles.textInputStyle}
+              multiline={true}
+              numberOfLines={3}
+              placeholder="Comment"
+            />
+          <TouchableOpacity onPress={() => navigation.navigate('MaterialList')}>
+            <Icon name="arrow-circle-up" size={40}  />
+          </TouchableOpacity>
+        </View>
+       
+        {
+          comment.map((value, index) => (
+            <View key={index}>
+              <Text>{value.created_at}</Text>
+              <View style={styles.comment}>
+                <Image source={{ uri: user.image }} style={{ width: 50, height: 50, borderRadius: 25 }} />
+                <View style={styles.commentDescription}>
+                  <Text style={styles.videoTitle}>{user.fullname}</Text>
+                  <Text style={styles.videoTitle}>{value.comment}</Text>
+                </View>
+              </View>
+            </View>
+
+          ))
+        }
+    </View>
+    </ScrollView>
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor:'#fff'
-    },
-    card: {
-        padding: 5,
-        marginTop: 20,
-    },
-    cardBody: {
-        marginBottom: 30,
-    },
-    cardheader: {},
-    title: {
-        fontSize: 20,
-        color: '#000',
-        fontWeight:'600'
-    },
-    text: {
-        color: '#000',
-        fontSize: 16,
-    },
-    image: {
-        width: '100%',
-        height: 200,
-        marginTop: 10,
-        marginBottom: 10,
-        borderRadius: 10,
-    },
-    imageProfile: {
-        width: 50,
-        height: 50,
-        marginTop: 10,
-        marginBottom: 10,
-        borderRadius: 50,
-        marginRight: 20,
-    },
-    btnBack: {
-        marginTop: 10,
-        padding: 3,
-        marginLeft: 5,
-        backgroundColor: '#04AA6D',
-        width: 70,
-        textAlign: 'center',
-        borderRadius: 10,
-        shadowColor: '#171717',
-        shadowOffset: { width: 0, height: 4 },
-        shadowOpacity: 0.2,
-        shadowRadius: 4,
-    },
-    des:{
-        marginTop:10,
-    },
-    textBtn:{
-        color:'#fff',
-        padding:5,
-        fontSize:16,
-        fontWeight:'500',
-        textAlign:'center'
-    }
+  container: {
+    backgroundColor: '#fff',
+    flex: 1,
+    padding: 15,
+  },
+  scrollView: {
+    marginHorizontal: 2,
+  },
+  row: {
+    flexDirection: 'row'
+  },
+  comment: {
+    flexDirection: 'row',
+    paddingTop: 15,
+    paddingBottom: 15,
+  },
+  commentDescription: {
+    paddingLeft: 10,
+  },
+  card: {
+    padding: 5,
+    marginTop: 5,
+  },
+  cardBody: {
+    marginBottom: 15,
+  },
+  cardheader: {},
+  title: {
+    fontSize: 20,
+    color: '#000',
+    fontWeight: '600'
+  },
+  text: {
+    color: '#000',
+    fontSize: 16,
+  },
+  image: {
+    width: '100%',
+    height: 200,
+    marginTop: 10,
+    marginBottom: 10,
+    borderRadius: 10,
+  },
+  imageProfile: {
+    width: 50,
+    height: 50,
+    marginTop: 10,
+    marginBottom: 10,
+    borderRadius: 50,
+    marginRight: 20,
+  },
+  btnBack: {
+    marginLeft: 5,
+    backgroundColor: '#04AA6D',
+    width: '15%',
+    textAlign: 'center',
+    height: 40,
+    textAlign: 'center',
+    borderRadius: 10,
+    shadowColor: '#171717',
+  },
+  des: {
+    marginTop: 10,
+  },
+  textBtn: {
+    color: '#fff',
+    padding: 5,
+    fontSize: 16,
+    fontWeight: '500',
+    textAlign: 'center'
+  },
+  textInputStyle: {
+    backgroundColor: "#E8E4E4",
+    padding: 10,
+    borderColor: '#000',
+    borderRadius: 10,
+    width: '85%',
+    marginBottom: 20,
+  },
 });
 
 export default MaterialDetail;
